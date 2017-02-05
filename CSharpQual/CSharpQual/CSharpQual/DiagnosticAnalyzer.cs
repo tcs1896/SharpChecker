fdcc398d876a5a8e7c9bcf3889129f50ae6d3825
Ted Sill <techsill@yahoo.com>
2017-02-05 00:03:19 -0500

    Analyzer which confirms the correct number of arguments have been provided to String.Format

--- CSharpQual/CSharpQual/CSharpQual/DiagnosticAnalyzer.cs
+++ CSharpQual/CSharpQual/CSharpQual/DiagnosticAnalyzer.cs
@@ -7,22 +7,20 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace CSharpQual
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class CSharpQualAnalyzer : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "CSharpQual";
+        public const string DiagnosticId = "CSQ_StringFormat";
+        internal const string Title = "Error in string format";
+        internal const string MessageFormat = "String format error {0}";
+        internal const string Description = "The number of arguments should match those referenced in the string.";
+        internal const string Category = "Syntax";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
-
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
@@ -30,22 +28,59 @@ namespace CSharpQual
         {
             // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
             // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSyntaxNodeAction<SyntaxKind>(AnalyzeNode, SyntaxKind.InvocationExpression);
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+            //We can safely cast here because we filter above when we register
+            var invocationExpr = (InvocationExpressionSyntax)context.Node;
+            //Attempt to grab a memberAccessExpr.  In this case Regex.Match
+            var memberAccessExpr = invocationExpr.Expression as MemberAccessExpressionSyntax;
+            //Checking the syntax is fast, and this is preferred as an initial mechanism becauase analyzers
+            //may be executed many times a second as text is entered into an editor
+            if (memberAccessExpr?.Name.ToString() != "Format") return;
+            //Now we know we have a 'Match' method invocation, so we incur the cost to get the associated symbol
+            var memberSymbol = context.SemanticModel.GetSymbolInfo(memberAccessExpr).Symbol as IMethodSymbol;
+            //If we are dealing with the correct namespace then bail
+            if (!memberSymbol?.ToString().StartsWith("string.Format") ?? true) return;
+            //Grab the argument list so we can interrogate it
+            var argumentList = invocationExpr.ArgumentList as ArgumentListSyntax;
+            //Make sure the first argument is a string literal.
+            var patternLiteral = argumentList.Arguments[0].Expression as LiteralExpressionSyntax;
+            if (patternLiteral == null) return;
+            //Now that we know its a literal we can retrieve the value
+            var patternOpt = context.SemanticModel.GetConstantValue(patternLiteral);
+            if (!patternOpt.HasValue) return;
+            var pattern = patternOpt.Value as string;
+            if (pattern == null) return;
 
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            //Dig into the first argument to see how many arguments are expected in the surface langauge
+            int maxValue = 0;
+            foreach (Match m in Regex.Matches(pattern, "{.*?}"))
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
+                string stringMatch = m.Value.Replace("{", String.Empty).Replace("}", String.Empty);
+                int thisValue;
+                if (Int32.TryParse(stringMatch, out thisValue))
+                {
+                    if (thisValue > maxValue)
+                    {
+                        maxValue = thisValue;
+                    }
+                }
+            }
+            //Now that we know the maximum value, check to make sure we have exactly that number of
+            //arguments in addition to the one which specifies the pattern
+            if(argumentList.Arguments.Count != (maxValue + 2))
+            {
+                //Create the appropriate diagnostic, span for the token we want to underline, and message
+                var diagnostic =
+                    Diagnostic.Create(Rule,
+                    patternLiteral.GetLocation(), Description);
+                //Now we register this diagnostic with visual studio
                 context.ReportDiagnostic(diagnostic);
             }
         }
+        
     }
 }
