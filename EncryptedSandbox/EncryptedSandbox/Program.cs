87ec586caf672d71e436cd72a1b3e4008c770183
Ted Sill <techsill@yahoo.com>
2017-02-21 19:13:29 -0500

    Simplfying the EncryptedSandbox target program so that it contains only the essential elements to demonstrate the functionality of the analayzer

--- EncryptedSandbox/EncryptedSandbox/Program.cs
+++ EncryptedSandbox/EncryptedSandbox/Program.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 namespace EncryptedSandbox
 {
-    [Encrypted]
     class Program
     {
         static void Main(string[] args)
@@ -20,13 +19,14 @@ namespace EncryptedSandbox
         public string Encrypt(string text)
         {
             string rtn = text;
+            // Performing the encryption
             return rtn;
         }
 
         // Only send encrypted data!
         public void sendOverInternet([Encrypted] String msg)
         {
-            // ...
+            // Send the data over an insecure medium
         }
 
         [Encrypted]
@@ -36,74 +36,19 @@ namespace EncryptedSandbox
 
         void sendText()
         {
-            // ...
             string plaintext = "Anyone can read this!";
+            //We want to verify this assignment.  The return type of Encrypt is annotated, and
+            //will match the annotation of Ciphertext, so this should be accepted
             Ciphertext = Encrypt(plaintext);
-
-            //Demonstrate how to retrieve the properties of a class and check for the custom attribute
-            Type t = typeof(Program);
-            var props = t.GetProperties();
-            foreach (PropertyInfo prop in props)
-            {
-                object[] attrs = prop.GetCustomAttributes(true);
-                foreach (object attr in attrs)
-                {
-                    EncryptedAttribute encrAttr = attr as EncryptedAttribute;
-                    if (encrAttr != null)
-                    {
-                        Console.WriteLine($"Property {prop.Name} has the {nameof(EncryptedAttribute)}");
-                    }
-                }
-            }
-
-            //Demonstrate how to investigate the methods of a class, seraching for the custome attribute
-            var methods = t.GetMethods();
-            foreach (var met in methods)
-            {
-                var attrs = met.GetCustomAttributes(true);
-                foreach (var attr in attrs)
-                {
-                    EncryptedAttribute encrAttr = attr as EncryptedAttribute;
-                    if (encrAttr != null)
-                    {
-                        Console.WriteLine($"Method {met.Name} has the {nameof(EncryptedAttribute)}");
-                    }
-                }
-
-                //Determine if the method arguments have the attribute assigned
-                var methodArgs = met.GetParameters();
-                foreach (var mArg in methodArgs)
-                {
-                    var mArgAttrs = mArg.GetCustomAttributes(true);
-                    foreach (var attr in mArgAttrs)
-                    {
-                        EncryptedAttribute encrAttr = attr as EncryptedAttribute;
-                        if (encrAttr != null)
-                        {
-                            Console.WriteLine($"Method {met.Name} has parameter {mArg.Name} with the {nameof(EncryptedAttribute)}");
-                        }
-                    }
-                }
-            }
 
             //This should be an allowed usage because Ciphertext has the [Encrypted] attribute
             //At this call site we need to determine that the method expects an value with an attribute, then determine if the value
             //being passed has this attribute (or eventually a subtype attribute).
             sendOverInternet(Ciphertext);
+            //This should generate an error because 'RawText' does not have the [Encrypted] attribute
             sendOverInternet(RawText);
+            //This is also unnacceptable
             sendOverInternet("");
         }
-
-        //private static void FindAttributes<T>(T methods) where T : IEnumerable<T>
-        //{
-
-        //}
-
-        //void sendPassword()
-        //{
-        //    String password = getUserPassword();
-        //    sendOverInternet(password);
-        //}
-
     }
 }
