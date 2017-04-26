c4a86554e2462e16d309e6fc53cd8a1c4567cc02
Ted Sill <techsill@yahoo.com>
2017-04-25 21:24:34 -0400

    Adding a couple open source target applications used in validating SharpChecker

---
+++ Sanfoundry/RijndaelEncryption/RijndaelEncryption/Program.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * C# Program to Encrypt/Decrypt using Rijndael Key
+ * http://www.sanfoundry.com/csharp-program-encrypt-decrypt-rijndael-key/
+ */
+using SharpChecker.Attributes;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+namespace RijndaelManage
+{
+    class RijndaelEncryption
+    {
+        public static void Main()
+        {
+            try
+            {
+
+                string original = "Data For Encryption!!!!!";
+                using (RijndaelManaged myRijndael = new RijndaelManaged())
+                {
+
+                    myRijndael.GenerateKey();
+                    myRijndael.GenerateIV();
+                    byte[] encrypted = EncryptStringToBytes(original, myRijndael.Key, myRijndael.IV);
+                    Debug.Assert(true, "encrypted:Encrypted");
+                    string aftdecryp = DecryptStringFromBytes(encrypted, myRijndael.Key, myRijndael.IV);
+                    Console.WriteLine("Original:   {0}", original);
+                    Console.WriteLine("After Decryption: {0}", aftdecryp);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+        }
+
+        [return:Encrypted]
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+            byte[] encrypted;
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            Debug.Assert(true, "encrypted:Encrypted");
+            return encrypted;
+        }
+
+        static string DecryptStringFromBytes([Encrypted] byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+            string plaintext = null;
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+        }
+    }
+}
